c47ec5b4861eacbe3c7533a219d206e05b7026b3
Sinar Pandora <SinarPandora@outlook.com>
2024-06-27 19:47:51 +0800

    侧边栏亚克力样式 + 图标

--- DragonSpiritsINFight/ViewModels/MainWindow/MainWindowViewModel.cs
+++ DragonSpiritsINFight/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DragonSpiritsINFight.ViewModels.MainWindow.Pages;
+using Material.Icons;
 
 namespace DragonSpiritsINFight.ViewModels.MainWindow;
 
@@ -11,18 +12,41 @@ namespace DragonSpiritsINFight.ViewModels.MainWindow;
 /// </summary>
 public partial class MainWindowViewModel : ViewModelBase
 {
-    [ObservableProperty] private bool _isSideMenuOpen = true;
+    #region SideNavLogic
+
+    [ObservableProperty] private bool _isSideNavOpen = true;
+
+    /// <summary>
+    ///     切换左侧导航栏显示状态
+    /// </summary>
+    [RelayCommand]
+    public void ToggleSideNav()
+    {
+        IsSideNavOpen = !IsSideNavOpen;
+    }
+
+    #region SwitchPageLogic
 
     [ObservableProperty] private ViewModelBase _currentPage = new HomePageViewModel();
 
+    /// <summary>
+    ///     可选择页面列表
+    /// </summary>
     public ObservableCollection<NavItemTemplate> NavItems { get; } =
     [
-        new NavItemTemplate(typeof(HomePageViewModel), "概览"),
-        new NavItemTemplate(typeof(PartyPageViewModel), "编队")
+        new NavItemTemplate(typeof(HomePageViewModel), "概览", MaterialIconKind.ViewDashboardOutline),
+        new NavItemTemplate(typeof(PartyPageViewModel), "编队", MaterialIconKind.ListBoxOutline)
     ];
 
+    /// <summary>
+    ///     选择的页面
+    /// </summary>
     [ObservableProperty] private NavItemTemplate? _selectNavItem;
 
+    /// <summary>
+    ///     当选择左侧项目时，改变右侧页面
+    /// </summary>
+    /// <param name="value">新页面</param>
     partial void OnSelectNavItemChanged(NavItemTemplate? value)
     {
         if (value is null) return;
@@ -31,9 +55,7 @@ public partial class MainWindowViewModel : ViewModelBase
         CurrentPage = (ViewModelBase)instance;
     }
 
-    [RelayCommand]
-    public void ToggleSideMenu()
-    {
-        IsSideMenuOpen = !IsSideMenuOpen;
-    }
+    #endregion
+
+    #endregion
 }
--- DragonSpiritsINFight/ViewModels/MainWindow/NavItemTemplate.cs
+++ DragonSpiritsINFight/ViewModels/MainWindow/NavItemTemplate.cs
@@ -1,12 +1,14 @@
 using System;
+using Material.Icons;
 
 namespace DragonSpiritsINFight.ViewModels.MainWindow;
 
 /// <summary>
 ///     导航项模板
 /// </summary>
-public class NavItemTemplate(Type viewModelType, string label)
+public class NavItemTemplate(Type viewModelType, string label, MaterialIconKind icon)
 {
     public Type ViewModelType { get; } = viewModelType;
     public string Label { get; } = label;
+    public MaterialIconKind Icon { get; } = icon;
 }
